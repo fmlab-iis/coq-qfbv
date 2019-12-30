ff1df55f77fd4ef98349c24166a247b5f3304e8d
Ming-Hsien Tsai <mhtsai208@gmail.com>
2019-12-30 09:48:47 +0800

    Add more extracted functions.

--- src/Extract.v
+++ src/Extract.v
@@ -17,7 +17,8 @@ Extraction Blacklist Nat Int List String.
 Cd "src/extraction".
 Separate Extraction
          seq.catrev nat_of_int n_of_int int_of_nat int_of_n
-         from_string from_hex from_bin to_hex
+         NBitsDef.from_string NBitsDef.from_hex NBitsDef.from_bin NBitsDef.from_Z
+         NBitsDef.to_string NBitsDef.to_hex NBitsDef.to_bin NBitsDef.to_Z
          bexp_to_cnf max_var_of_cnf num_clauses dimacs_cnf_with_header.
 Cd "../..".
 
